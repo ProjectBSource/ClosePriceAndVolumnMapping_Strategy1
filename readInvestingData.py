cda0b3aaa1c51368522c66a2e7d675733aac31f9
ProjectB <57570834+ProjectBSource@users.noreply.github.com>
2023-08-20 02:25:32 +0800

    Update readInvestingData.py

--- readInvestingData.py
+++ readInvestingData.py
@@ -1,6 +1,8 @@
 import csv
 import math
+import statistics
 
+from scipy.stats import zscore
 
 dates = []
 close_prices = []
@@ -10,8 +12,7 @@ lows = []
 volumes = []
 percentage_changes = []
 
-csv_file_path = '/Users/patrickfung/Downloads/DowJonesHistorialData.csv'
-outputFile  = '/Users/patrickfung/Downloads/outputFile.csv'
+csv_file_path = './DowJonesHistorialData.csv'
 
 def read_csv_file(file_path):
     with open(file_path, 'r') as file:
@@ -43,25 +44,46 @@ def read_csv_file(file_path):
     #print("Lows:", lows)
     #print("Volumes:", volumes)
     #print("Percentage Changes:", percentage_changes)
-    
-def calculateEachClosePriceSumupVolumn(range):
-    min_close_price = int(min(close_prices))
-    max_close_price = int(max(close_prices))
-    arraySize = math.ceil((max_close_price-min_close_price)/range)
-    close_price_list = [0] * arraySize
+        
+def calculateEachIntervalePriceSumupVolumn(interval):
+    min_close_price = math.floor(min(close_prices)/interval)*interval
+    max_close_price = math.ceil(max(close_prices)/interval)*interval
+    arraySize = math.ceil((max_close_price-min_close_price)/interval)
+    sumUPList = [0] * arraySize
     for i in range(0, len(dates), 1):
         tempVolumns = volumes[i].replace("K", "")
         if(tempVolumns != None and tempVolumns != ""):
             tempVolumns = float(volumes[i].replace("K", ""))*1000
-            arrayPosition = math.floor( int(close_prices[i] - min_close_price) / range)
-            close_price_list[ arrayPosition ] += tempVolumns
+            arrayPosition = math.floor(int(close_prices[i] - min_close_price)/interval)
+            if(arrayPosition==9):
+                print()
+            sumUPList[ arrayPosition ] += tempVolumns
+    outputFile  = './outputFile(calculateEachIntervalePriceSumupVolumn).csv'
     with open(outputFile, 'w') as file:
-        for i in range(len(close_price_list)-1, 0, -1):
-            file.write("\"{a}\",\"{b}\"\n".format(a = str(min_close_price+i), b = str(close_price_list[i])))
-        
+        for i in range(len(sumUPList)-1, -1, -1):
+            file.write("\"{a}\",\"{b}\"\n".format(a = str(min_close_price+(i*interval)), b = str(sumUPList[i])))
+    return sumUPList
+            
+
+def calculateZscore(data):
+    #caluculate the mean first
+    mean = 0;
+    for i in range(len(data)-1, -1, -1):
+        mean += data[i]
+    mean = mean / len(data)
     
+    #setup standard_deviation
+    standard_deviation = 0.1
     
+    outputFile  = './outputFile(calculateZscore).csv'
+    with open(outputFile, 'w') as file:
+        for i in range(len(data)-1, -1, -1):
+            z_scores = statistics.NormalDist(mean, standard_deviation).zscore(data[i])
+            file.write("\"{a}\",\"{b}\"\n".format(a = str(data[i]), b = str(z_scores)))
+    return z_scores
+
 
 # Example usage
 read_csv_file(csv_file_path)
-calculateEachClosePriceSumupVolumn(1);
+result1 = calculateEachIntervalePriceSumupVolumn(10);
+result2 = calculateZscore(result1);
