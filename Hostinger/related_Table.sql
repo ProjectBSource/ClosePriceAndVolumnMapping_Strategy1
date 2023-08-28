0a271c53ed20d2b379c3f5db96ec8957b29a8d79
ProjectB <57570834+ProjectBSource@users.noreply.github.com>
2023-08-28 10:45:59 +0800

    Create related_Table.sql

---
+++ Hostinger/related_Table.sql
@@ -0,0 +1,9 @@
+--
+-- 資料表結構 `patrick_strategy_StrongVolumn`
+--
+
+CREATE TABLE `patrick_strategy_StrongVolumn` (
+  `symbol` varchar(20) NOT NULL,
+  `positions` varchar(100) NOT NULL,
+  `checkdatetime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
